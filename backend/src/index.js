3439c4bf9e28f8f298140e249c0043057591d8c1
Ayush-3012 <diveshsrivastava31@gmail.com>
2025-06-26 23:27:30 +0530

    fixed some bugs

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,11 @@ dotenv.config();
 import app from "./app.js";
 
 app.get("/", (req, res) => res.send("Hello, Welcome!"));
+const PORT = 5000;
 
 import todoRouter from "./routes/todo.routes.js";
-app.use("/api/v1/todos", todoRouter)
+app.use("/api/v1/todos", todoRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(` Server is listening to port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(` Server is listening to port ${PORT}`);
 });
